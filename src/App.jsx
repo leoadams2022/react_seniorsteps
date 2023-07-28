c2b187f47aa28a1d05bf9335bc15e110cfd61dd4
leoadams2022 <sam0941535901@gmail.com>
2023-07-28 21:30:05 +0200

    fisrt commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-import Navbar from './components/Navbar'
-import Header from './components/Header'
-import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Navbar />
-      <Header />
-      <Footer />
+      <div>App</div>
     </>
   )
 }
