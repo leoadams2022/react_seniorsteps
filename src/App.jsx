6760de758790cba20e6c00b3732ab8fe850a6ee4
leoadams2022 <sam0941535901@gmail.com>
2023-07-28 21:43:39 +0200

    session_2 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>App</div>
+      <div className='app'>
+        <Header />
+      </div>
     </>
   )
 }
