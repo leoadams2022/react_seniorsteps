b2f76ae15d5021ec7a37280a66dae15b28e02f61
leoadams2022 <sam0941535901@gmail.com>
2023-07-28 21:23:26 +0200

    session_1 done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div className='footer'>
+            <ul className="footerList1">
+                <li>name</li>
+                <li>Email</li>
+                <li>Address</li>
+            </ul>
+            <ul className="footerList1">
+                <li>Position</li>
+                <li>Prices</li>
+                <li>Phone</li>
+            </ul>
+        </div>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div className='header'>
+            <div className="box">
+                <p className="boxTitle">Lorem, ipsum.</p>
+                <p className="boxText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima deleniti reiciendis neque eveniet ad ratione in, sit aut incidunt est!</p>
+            </div>
+            <div className="box">
+                <p className="boxTitle">Lorem, ipsum.</p>
+                <p className="boxText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima deleniti reiciendis neque eveniet ad ratione in, sit aut incidunt est!</p>
+            </div>
+            <div className="box">
+                <p className="boxTitle">Lorem, ipsum.</p>
+                <p className="boxText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima deleniti reiciendis neque eveniet ad ratione in, sit aut incidunt est!</p>
+            </div>
+            <div className="box">
+                <p className="boxTitle">Lorem, ipsum.</p>
+                <p className="boxText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima deleniti reiciendis neque eveniet ad ratione in, sit aut incidunt est!</p>
+            </div>
+            <div className="box">
+                <p className="boxTitle">Lorem, ipsum.</p>
+                <p className="boxText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima deleniti reiciendis neque eveniet ad ratione in, sit aut incidunt est!</p>
+            </div>
+            <div className="box">
+                <p className="boxTitle">Lorem, ipsum.</p>
+                <p className="boxText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima deleniti reiciendis neque eveniet ad ratione in, sit aut incidunt est!</p>
+            </div>
+            <div className="box">
+                <p className="boxTitle">Lorem, ipsum.</p>
+                <p className="boxText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima deleniti reiciendis neque eveniet ad ratione in, sit aut incidunt est!</p>
+            </div>
+            <div className="box">
+                <p className="boxTitle">Lorem, ipsum.</p>
+                <p className="boxText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima deleniti reiciendis neque eveniet ad ratione in, sit aut incidunt est!</p>
+            </div>
+        </div>
+    )
+}
