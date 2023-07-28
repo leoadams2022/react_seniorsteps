4043c2e3c6b2871fc9a9d47ab7d99af1c73a269a
leoadams2022 <sam0941535901@gmail.com>
2023-07-28 22:54:32 +0200

    session_3 done

--- src/components/People.jsx
+++ src/components/People.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Person from './Person'
 
 export default function People({ data }) {
+    const [searchStr, setSearchStr] = useState(null);
+    const handelSearch = (e) => {
+        setSearchStr(e.target.value);
+    }
     return (
         <div>
+            <input type="text" onChange={(e) => handelSearch(e)} placeholder='search by name' />
             {
-                data.map((person, index) => {
+                (searchStr === '' ? data : data.filter((person) => (person.name === searchStr))).map((person, index) => {
                     return (
                         <div key={index}>
                             <Person name={person.name} age={person.age} email={person.email} number={person.number} />
